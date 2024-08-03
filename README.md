7a50f2a710ca71a05f612e53a9d5e19d603f14e4
KalebAsratemedhin <risekab@gmail.com>
2024-08-03 21:36:50 +0300

    add screenshots

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 This is a simple todos web app done using html, css and javascript. Users can add todos, edit them or delete them. It stores the todos in memory so if the page is refreshed, they will be lost.
 
 # How to run 
-You can run the app using liveserver or by clicking on the file and opening it on your browser.
+You can run the app using liveserver or open the index.html file with a browser.
+
+### Add a todo
+
+![alt text](<Screenshot from 2024-08-03 21-32-58.png>)
+
+### Edit a todo
+![alt text](<Screenshot from 2024-08-03 21-33-20.png>)
+![alt text](<Screenshot from 2024-08-03 21-33-26.png>)
+
+### Delete a todo
+![alt text](<Screenshot from 2024-08-03 21-33-42.png>)
--- index.js
+++ index.js
@@ -5,93 +5,94 @@ const todoBox = document.querySelector(".my-todos")
 
 let todos = []
 
-addButton.onclick = () => {
-    if(todoInput.value){
-        todos.push(todoInput.value)
-        showTodo(todoInput.value)
+addButton.onclick = () => addTodo(todoInput.value)
 
+
+function addTodo(todo){
+    if(todo){
+        todos.push(todo)
+        todoInput.value = ""
     }
+    todoBox.innerHTML = ""
+    showTodos()
 
 }
 
 
+function editTodo(index, itemList, itemEdit, saveButton, editButton){
+    // hide the edit button and the list element instead show input field and save button
+    itemList.style = "display: none;"
+    itemEdit.style = "display: inline"
+    saveButton.style = "display: inline"
+    editButton.style = "display: none"
 
+}
 
+function updateTodo(index, itemList, itemEdit, saveButton, editButton){
+    itemList.textContent = itemEdit.value
 
+    itemList.style = "display: inline;"
+    itemEdit.style = "display: none"
+    saveButton.style = "display: none"
+    editButton.style = "display: inline"
 
-const showTodo = (todo) => {
-    const item = document.createElement("div")
-    const buttons = document.createElement("div")
-    const deleteButton = document.createElement("button")
-    const editButton = document.createElement("button")
-    const saveButton = document.createElement("button")
+    // update todo in array
+    todos[index] = itemEdit.value
 
-    const itemList = document.createElement("li")
-    itemList.textContent = todo
-
-    const itemEdit = document.createElement("input")
-    itemEdit.value = todo 
-
-    itemList.style = "list-style: none; display: inline;"
-    itemEdit.style = "display: none;"
     
-    item.appendChild(itemList)
-    item.appendChild(itemEdit)
-    item.style = "border: 1px solid black; border-radius: 3px; padding: 10px; height: 50px; width: 50%; box-shadow: 0.5px 1px 1px 0.5px; display: flex; justify-content: space-between; align-items: center; margin-bottom: 5px;"
-   
+}
 
+function deleteTodo(todoIndex, item){
+    todos = todos.filter((value, index) => index != todoIndex )
+    todoBox.removeChild(item)
 
-    deleteButton.textContent = "delete"
-    editButton.textContent = "edit"
-    saveButton.textContent = "save"
+}
 
-    buttons.style = "min-width: 100px; display: flex; justify-content: space-between;"
-    saveButton.style = "display: none;"
+function showTodos(){
 
-    deleteButton.onclick = () => {
-        todos = todos.filter(tod => tod != todo )
-        todoBox.removeChild(item)
-    }
-
-    editButton.onclick = () => {
-
-        itemList.style = "display: none;"
-        itemEdit.style = "display: inline"
-        saveButton.style = "display: inline"
-        editButton.style = "display: none"
+    todos.forEach((todo, index) => {
+        const item = document.createElement("div")
+        const buttons = document.createElement("div")
+        const deleteButton = document.createElement("button")
+        const editButton = document.createElement("button")
+        const saveButton = document.createElement("button")
+    
+        const itemList = document.createElement("li")
+        itemList.textContent = todo
+    
+        const itemEdit = document.createElement("input")
+        itemEdit.value = todo 
+    
+        itemList.style = "list-style: none; display: inline;"
+        itemEdit.style = "display: none;"
         
-    }
-
-    saveButton.onclick = () => {
-
-        itemList.textContent = itemEdit.value
-
-        itemList.style = "display: inline;"
-        itemEdit.style = "display: none"
-        saveButton.style = "display: none"
-        editButton.style = "display: inline"
-
-        for (i = 0; i < todos.length; i++){
-            if (todos[i] === todo){
-                todos[i] = itemEdit.value
-            }
-        }
-
-
-    }
+        item.appendChild(itemList)
+        item.appendChild(itemEdit)
+        item.style = " border-radius: 3px; padding: 10px; height: 50px; width: 50%; box-shadow: 1px 1px 1px 1px grey; display: flex; justify-content: space-between; align-items: center; margin-bottom: 7px;"
+       
+    
+    
+        deleteButton.textContent = "delete"
+        editButton.textContent = "edit"
+        saveButton.textContent = "save"
 
     
+        buttons.style = "min-width: 100px; display: flex; justify-content: space-between;"
+        saveButton.style = "display: none;"
 
-    buttons.appendChild(deleteButton)
-    buttons.appendChild(editButton)
-    buttons.appendChild(saveButton)
+        editButton.onclick = () => editTodo(index, itemList, itemEdit, saveButton, editButton)
+        deleteButton.onclick = () => deleteTodo(index, item) 
+        saveButton.onclick = () => updateTodo(index, itemList, itemEdit, saveButton, editButton)
 
-    item.appendChild(buttons)
-
-
-
-    todoBox.appendChild( item)
+        buttons.appendChild(deleteButton)
+        buttons.appendChild(editButton)
+        buttons.appendChild(saveButton)
     
-
+        item.appendChild(buttons)
+    
+        todoBox.appendChild( item)
+    
+        
+    });
 
 }
